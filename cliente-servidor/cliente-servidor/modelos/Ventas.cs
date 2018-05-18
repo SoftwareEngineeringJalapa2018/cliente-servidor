345596753847fdb3e8692e08a3b6cd156f4d6c8f
eduardo19Fu <ramieduar@gmail.com>
2018-05-18 13:11:12 -0600

    Agregado de clases de ventas y su respectivo controlador

---
+++ cliente-servidor/cliente-servidor/modelos/Ventas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cliente_servidor.modelos
+{
+    class Ventas
+    {
+        public int id { get; set; }
+        public string nombreProducto { get; set; }
+        public int stuck { get; set;}
+        public DateTime fechaVendido { get; set; }
+        public string nombreCliente { get; set; }
+    }
+}
